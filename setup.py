467c1a1fe8d92cdc21d6b88a00798f6ceb0f0d98
Mino Ku <happykus@naver.com>
2016-03-23 11:26:20 +0900

    Modify setup.py. Remove useless files.

--- setup.py
+++ setup.py
@@ -11,4 +11,5 @@ setup (
 	license		= 'MIT Lincense',
 	keywords	= ['fixqueue','queue', 'list', 'collection','simplequeue','fixed size queue', 'fix queue'],
 	description	= 'Simplest Queue module to make a fixed size queue on your platform and apps. You just need to use FixQueue-based queues like normal list-based objects.'
+	classifiers	= ['License :: OSI Approved :: MIT License', 'Programming Language :: Python', 'Programming Language :: Python :: Implementation', 'Programming Language :: Python :: Implementation :: Stackless', 'Topic :: Software Development', 'Topic :: Software Development :: Libraries'],
 	)
